4c2db6ad342dbc4d1e64fb990c7c74e4471b9547
Ferdinand Malcher <ferdinand@malcher.media>
2020-08-19 13:23:18 +0200

    add book-rating

---
+++ book-rating/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'br-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Book Rating';
+
+  /*constructor() {
+    setTimeout(() => {
+      this.title = 'Hallo Welt';
+    }, 2000);
+  }*/
+}
