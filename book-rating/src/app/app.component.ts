e40fe47b7598cf53d0d5afcb9a386c065140c6c5
Ferdinand Malcher <ferdinand@malcher.media>
2020-08-25 13:42:36 +0200

    Fun with observables

--- book-rating/src/app/app.component.ts
+++ book-rating/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Observable, of, timer } from 'rxjs';
+import { map, filter } from 'rxjs/operators';
 
 @Component({
   selector: 'br-root',
@@ -8,9 +10,57 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Book Rating';
 
-  /*constructor() {
-    setTimeout(() => {
-      this.title = 'Hallo Welt';
-    }, 2000);
-  }*/
+  constructor() {
+
+    const myObs$ = new Observable(obs => {
+      obs.next('A');
+      obs.next('B');
+      obs.next('C');
+      obs.complete();
+      // of('A', 'B', 'C');
+    });
+
+    // myObs$.subscribe(e => console.log(e));
+
+    // import { of, timer } from 'rxjs';
+    // of(1,2,3,4,5,6,7)
+
+    // import { map } from 'rxjs/operators';
+    timer(0, 1000).pipe(
+      map(e => e * 3),
+      filter(e => e % 2 === 0)
+    ).subscribe({
+      next: e => console.log(e),
+      error: err => console.error(err),
+      complete: () => console.log('COMPLETE')
+    });
+
+
+
+    // Producer: Funktion, die Werte generiert
+    // Producer kapseln in new Observable()
+    // Producer: Argument Observer
+    // Producer: ruft Methoden auf Observer auf
+
+    // Observable subscriben => Observer oder Callbacks
+
+    function producer(obs) {
+      obs.next(1);
+
+      setTimeout(() => {
+        obs.next(3);
+        obs.complete();
+      }, 2000);
+    }
+
+    const observer = {
+      next: e => console.log(e),
+      error: err => console.error(err),
+      complete: () => console.log('COMPLETE')
+    };
+
+    const myObservable$ = new Observable(producer);
+    // myObservable$.subscribe(observer);
+    // myObservable$.subscribe(e => console.log(e));
+  }
 }
