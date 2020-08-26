a16be38563539584ed55f9c04001c2e057abe2f1
Ferdinand Malcher <ferdinand@malcher.media>
2020-08-26 11:39:31 +0200

    RxJS: Chat, Errors, Memory Leaks, Higher-Order

--- rxjs-playground/src/app/exercises/error-handling/error-handling.component.ts
+++ rxjs-playground/src/app/exercises/error-handling/error-handling.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { ReplaySubject, throwError, of } from 'rxjs';
+import { ReplaySubject, throwError, of, EMPTY } from 'rxjs';
 import { retry, catchError } from 'rxjs/operators';
 
 import { ExerciseService } from '../exercise.service';
@@ -15,7 +15,15 @@ export class ErrorHandlingComponent {
   constructor(private es: ExerciseService) { }
 
   start() {
-    this.es.randomError().subscribe({
+    this.es.randomError().pipe(
+      // retry(3)
+      catchError(err => {
+        console.log('FEHLER:', err);
+        // return throwError('Böser Fehler!'); // Fehler weiterwerfen
+        return of('Kein', 'Fehler', 'passiert'); // Fehler umwandeln
+        // return EMPTY; // Fehler verschlucken
+      })
+    ).subscribe({
       next: value => this.logStream$.next(value),
       error: err => this.logStream$.next('💥 ERROR: ' + err)
     });
